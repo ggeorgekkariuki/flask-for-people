7a9b78afbd6a208ffa4e1bdd25c8427448e0978c
George Gachugu Kariuki <gkariuki@ihlkenya.co.ke>
2019-11-22 11:28:20 +0300

    Add line to point to swagger.yml file

--- hello.py
+++ hello.py
@@ -9,10 +9,13 @@ import connexion
 
 app = connexion.App(__name__, specification_dir='./')
 
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
